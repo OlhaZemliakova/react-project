ff0802e232ca3344112b5053b22b5d54e5647822
Olha Zemliakova <zemlakova@gmail.com>
2023-02-17 16:02:15 -0800

    init

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import axios from "axios";
+import React, { useMemo, useState } from "react";
+import PostFilter from "./components/PostFilter";
+import PostForm from "./components/PostForm";
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyModal from "./components/UI/myModal/MyModal";
+import { usePosts } from "./hooks/usePosts";
+import "./styles/App.css";
+
+function App() {
+  const [posts, setPosts] = useState([]);
+
+  const [filter, setFilter] = useState({ sort: "", query: "" });
+  const [modal, setModal] = useState(false);
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost]);
+    setModal(false);
+  };
+
+  async function fetchPosts() {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/posts"
+    );
+    setPosts(response.data);
+  }
+
+  const removePost = (post) => {
+    setPosts(posts.filter((item) => item.id !== post.id));
+  };
+
+  return (
+    <div className="App">
+      <MyButton onClick={fetchPosts}>Get posts</MyButton>
+      <MyButton style={{ marginTop: 30 }} onClick={() => setModal(true)}>
+        Create post
+      </MyButton>
+      <MyModal visible={modal} setVisible={setModal}>
+        <PostForm create={createPost} />
+      </MyModal>
+      <hr style={{ margin: "15px 0" }} />
+      <PostFilter filter={filter} setFilter={setFilter} />
+      <PostList
+        remove={removePost}
+        posts={sortedAndSearchedPosts}
+        title="List of posts:"
+      />
+    </div>
+  );
+}
+
+export default App;
